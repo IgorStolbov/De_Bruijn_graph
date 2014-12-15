892a95e1e5e3c23a11f0827179ecee2d292abf2b
IgorStolbov <igor.stolbov@yandex.ru>
2014-12-15 13:17:08 +0400

    граф строится

--- De_Bruijn_graph.py
+++ De_Bruijn_graph.py
@@ -1,44 +1,41 @@
 import networkx as nx
-import matplotlib as plt
+from numpy import *
+from networkx_viewer import Viewer
 
-n = 12
+n = 8
 m = 2
 a = 2
 
-getbit = lambda x, i: (x >> i) & 1
-
-class bitnode:
-    value = 0
-    def __init__(self, n):
-        self.value = n
-    def sum(self, t):
-        result = self.value ^ t.value
-        return bitnode(result)
-    def mult(self, t):
-        s1 = [int(i) for i in bin(self.value)[2:]]
-        s2 = [int(i) for i in bin(t.value)[2:]]
-        result = []
-        for i, j in enumerate(s1[::-1]):
-            if j != 0:
-                result.append(0)
-        return result
-
-class DBgraph:
-    G=nx.MultiGraph()
-    def build(self):
-        G = self.G
-        for i in range(2**n):
-            G.add_node(bitnode(i))
-        for i in G:
-            G.add_edge(i, i.mult(m))
-            G.add_edge(i, (i.mult(m)).sum(a))
-        return
-    def show(self):
-        return
+def mult(a, b):
+    result = 0
+    carry = 0
+    for counter in range(n):
+        if b & 1:
+            result ^= a
+        carry = a & (2**(n-1))  # detect if x^n term is about to be generated
+        a <<= 1
+        if carry:
+            a ^= 0x1B #replace x^20 with x^3 + 1
+        b >>= 1
+    start = 2
+    size = len(bin(result))
+    if size-2 > n:
+        start += size-2-n
+    result = int((bin(result))[start:], 2)
+    return result
 
 def main():
-    n = int("1000000", 2)
-    print(n)
-    print(getbit(n,3))
+    G=nx.MultiGraph()
+    for i in range(2**n):
+        G.add_node(i)
+    for i in G:
+        b = mult(i, m)
+        G.add_edge(i, b)
+        G.add_edge(i, a^b)
+#    nx.draw(G)
+#    nx.draw(G,pos=nx.spectral_layout(G), nodecolor='r',edge_color='b')
+    app = Viewer(G)
+    app.mainloop()
     return
+
 main()
